60b9f19632c4344b85686c24b230e045d478de2c
Alex <alexlong2001@outlook.com>
2024-08-09 21:50:29 +0100

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
